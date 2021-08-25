d0fbd37f7f3246a81c814b3bee792ca203425ce7
Andre Ziviani <andrepziviani@gmail.com>
2021-08-25 18:32:12 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.9.6-alpine3.13
+
+WORKDIR /app
+
+COPY src/. .
+
+RUN adduser -D app
+RUN pip install -r requirements.txt && \
+    rm requirements.txt
+
+RUN chown -R app:app /app
+
+USER app
+
+CMD ["python", "-u", "main.py"]
+
+HEALTHCHECK --timeout=10s CMD wget --no-verbose --tries=1 --spider http://localhost:${METRICS_PORT:=9798}/
---
+++ src/main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import datetime
+import os
+from prometheus_client import make_wsgi_app, Info
+from flask import Flask
+from waitress import serve
+import dns.resolver
+
+app = Flask("PublicIP-Exporter")  # Create flask app
+
+# Create Metrics
+ipv4 = Info('public_ip_v4', 'Public IPv4')
+ipv6 = Info('public_ip_v6', 'Public IPv6')
+
+
+def log(msg):
+    current_dt = datetime.datetime.now()
+    print(current_dt.strftime("%d/%m/%Y %H:%M:%S - ") + msg)
+
+
+def get_ip():
+    my_ipv4 = {}
+    my_ipv6 = {}
+
+    opendns_v4 = dns.resolver.resolve('resolver1.opendns.com.', 'A')
+    opendns_v6 = dns.resolver.resolve('resolver1.opendns.com.', 'AAAA')
+
+    resolver = dns.resolver.Resolver()
+
+    try:
+        resolver.nameservers = [opendns_v4[0].to_text()]
+        my_ipv4["address"] = resolver.resolve('myip.opendns.com')[0].to_text()
+    except dns.exception.Timeout:
+        log("ERROR: Timeout querying the IPv4 DNS server")
+        my_ipv4["address"] = "<Timeout>"
+
+    try:
+        resolver.nameservers = [opendns_v6[0].to_text()]
+        my_ipv6["ipv6"] = resolver.resolve('myip.opendns.com')[0].to_text()
+    except dns.exception.Timeout:
+        log("ERROR: Timeout querying the IPv6 DNS server")
+        my_ipv6["address"] = "<Timeout>"
+
+    return my_ipv4, my_ipv6
+
+
+@app.route("/metrics")
+def updateResults():
+    my_ipv4, my_ipv6 = get_ip()
+    ipv4.info(my_ipv4)
+    ipv6.info(my_ipv6)
+    current_dt = datetime.datetime.now()
+    print(
+        current_dt.strftime("%d/%m/%Y %H:%M:%S - ") +
+        f'My current ipv4 is {my_ipv4["address"]} and ipv6 is {my_ipv6["address"]}'
+    )
+    return make_wsgi_app()
+
+
+@app.route("/")
+def mainPage():
+    return ("<h1>Welcome to PublicIP-Exporter.</h1>" +
+            "Click <a href='/metrics'>here</a> to see metrics.")
+
+
+if __name__ == '__main__':
+    PORT = os.getenv('METRICS_PORT', 9798)
+    print("Starting PublicIP-Exporter on http://localhost:" + str(PORT))
+    serve(app, host='0.0.0.0', port=PORT)
